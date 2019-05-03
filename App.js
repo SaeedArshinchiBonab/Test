fa54e17810dd9c6e3663268b20a441e072585f2d
saeed arshinchi <arshinchi.bonab@gmail.com>
2019-05-03 13:44:23 +0430

    Modal Test in App.js

--- App.js
+++ App.js
@@ -10,34 +10,47 @@ import {
   WebView,
   Platform,
   ActivityIndicator,
-  Slider
+  Slider,
+  Modal,
+
 } from 'react-native';
 
 
 export default class App extends Component {
-constructor(props){
-  super(props);
-  this.state={
-    Slider_Value:0
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+    }
   }
-}
+  ShowModalFunction(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-      <Text style={{fontSize:20}}>Slider Value : {this.state.Slider_Value}</Text> 
-      <Slider 
-style={{width:'90%'}}
-step={1}
-minimumValue={0}
-maximumValue={100}
+        <Modal
+          transparent={false}
+          animationType="slide"
+          visible={this.state.modalVisible}
+          onRequestClose={() => this.state.ShowModalFunction(!this.state.modalVisible)}
 
-maximumTrackTintColor='#009688'
-onValueChange={(ChangeValue)=>this.setState({Slider_Value:ChangeValue})}
-      />
+        >
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+            <View style={styles.ModalInsideView}>
+              <Text style={styles.TextStyle}>
+                Text component with a sample text
+            </Text>
+              <Button title='بستن Modal' onPress={() => this.ShowModalFunction(!this.state.modalVisible)}></Button>
 
+            </View>
+          </View>
+        </Modal>
+        <Button title="نمایش Modal" onPress={() => this.ShowModalFunction(true)} />
       </View>
 
-      );
+    );
   }
 }
 
@@ -46,6 +59,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  ModalInsideView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00BCD4',
+    height: 300,
+    width: '90%',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  TextStyle: {
+    fontSize:24,
+    marginBottom:20,
+    color:'#fff',
+    padding:20,
+    textAlign: 'center'
   },
   WebViewStyle: {
     flex: 1,
