b16df48cf2f52792877d8082bf4d264896e834ac
saeed arshinchi <arshinchi.bonab@gmail.com>
2019-05-03 11:03:08 +0430

    Chenge App.js For test textInput mobile

--- App.js
+++ App.js
@@ -1,31 +1,74 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  TextInput,
+  Image,
+  Alert
+} from 'react-native';
 
-class TextComponent extends Component {
-  render() {
-    return (
-      <Text styles={{ fontFamily: 'IRANSansMobile', fontSize: 22 }}>
-        Hello {this.props.name}
-      </Text>
-    );
-  }
-}
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={title:'Salam'}
+    this.state = {
+      TextInputValue: ''
+    }
+  }
+  checkValue = () => {
+    if (this.state.TextInputValue == '') {
+      Alert.alert('لطفا شماره همراه خود را وارد کنید')
+      return false;
+    }
   }
   render() {
     return (
-      <View>
-        <TextComponent name="saeed" />
-        <TextComponent name="reza" />
-        <Text>
-          {this.state.title}
-        </Text>
-        <Button title="Click" onPress={()=>this.setState({title:''})}></Button>
+      <View style={styles.container}>
+        <View style={styles.sectionStyle}>
+          <Image source={require('./image/person_icon.png')} style={styles.imageStyle} />
+          <TextInput
+            style={{ flex: 1 }}
+            placeholder="شماره موبایل خود را وارد کنید"
+            underlineColorAndroid='transparent'
+            keyboardType='numeric'
+            value={this.state.TextInputValue}
+            onChangeText={(value) => this.setState({ TextInputValue: value })}
+          />
+        </View>
+        <Button
+          title="ذخیره اطلاعات"
+          onPress={this.checkValue}
+          color='#2196f3'
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  sectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#000',
+    height: 40,
+    borderRadius: 5,
+    margin: 10
+  },
+  imageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  }
+})
