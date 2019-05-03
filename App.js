66d4f42880602d3f41e2151ed84b67e7c7f1dc93
saeed arshinchi <arshinchi.bonab@gmail.com>
2019-05-03 11:57:41 +0430

    Slider Test in App.js

--- App.js
+++ App.js
@@ -9,50 +9,57 @@ import {
   Alert,
   WebView,
   Platform,
-  ActivityIndicator
+  ActivityIndicator,
+  Slider
 } from 'react-native';
 
 
 export default class App extends Component {
-  ActivityIndicatorLodingView(){
-    return (
-      <ActivityIndicator 
-        color='#002688'
-        size='large'
-        style={styles.ActivityIndicatorStyle}
-      />
-    )
+constructor(props){
+  super(props);
+  this.state={
+    Slider_Value:0
   }
-
+}
   render() {
     return (
-      <WebView
-        style={styles.WebViewStyle}
-        source={{uri:'http:reactapp.ir'}}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        renderLoading={this.ActivityIndicatorLodingView}
+      <View style={styles.container}>
+      <Text style={{fontSize:20}}>Slider Value : {this.state.Slider_Value}</Text> 
+      <Slider 
+style={{width:'90%'}}
+step={1}
+minimumValue={0}
+maximumValue={100}
 
+maximumTrackTintColor='#009688'
+onValueChange={(ChangeValue)=>this.setState({Slider_Value:ChangeValue})}
       />
-    );
+
+      </View>
+
+      );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   WebViewStyle: {
-    flex:1,
-    justifyContent : 'center',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop:(Platform.OS)==='ios'?20:0
+    marginTop: (Platform.OS) === 'ios' ? 20 : 0
   },
   ActivityIndicatorStyle: {
     position: 'absolute',
-    left:0,
-    right:0,
-    top:0,
-    bottom:0,
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems: 'center'
   }
 })
