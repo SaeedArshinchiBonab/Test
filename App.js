0aa3a5da285d897dc457bb18c0fb6e21d9910c8b
saeed arshinchi <arshinchi.bonab@gmail.com>
2019-05-03 11:37:02 +0430

    Test WebView in App.js

--- App.js
+++ App.js
@@ -6,69 +6,53 @@ import {
   View,
   TextInput,
   Image,
-  Alert
+  Alert,
+  WebView,
+  Platform,
+  ActivityIndicator
 } from 'react-native';
 
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      TextInputValue: ''
-    }
+  ActivityIndicatorLodingView(){
+    return (
+      <ActivityIndicator 
+        color='#002688'
+        size='large'
+        style={styles.ActivityIndicatorStyle}
+      />
+    )
   }
-  checkValue = () => {
-    if (this.state.TextInputValue == '') {
-      Alert.alert('لطفا شماره همراه خود را وارد کنید')
-      return false;
-    }
-  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.sectionStyle}>
-          <Image source={require('./image/person_icon.png')} style={styles.imageStyle} />
-          <TextInput
-            style={{ flex: 1 }}
-            placeholder="شماره موبایل خود را وارد کنید"
-            underlineColorAndroid='transparent'
-            keyboardType='numeric'
-            value={this.state.TextInputValue}
-            onChangeText={(value) => this.setState({ TextInputValue: value })}
-          />
-        </View>
-        <Button
-          title="ذخیره اطلاعات"
-          onPress={this.checkValue}
-          color='#2196f3'
-        />
-      </View>
+      <WebView
+        style={styles.WebViewStyle}
+        source={{uri:'http:reactapp.ir'}}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        renderLoading={this.ActivityIndicatorLodingView}
+
+      />
     );
   }
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  sectionStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+  WebViewStyle: {
+    flex:1,
+    justifyContent : 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
-    borderWidth: 2,
-    borderColor: '#000',
-    height: 40,
-    borderRadius: 5,
-    margin: 10
+    marginTop:(Platform.OS)==='ios'?20:0
   },
-  imageStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: 'stretch',
-    alignItems: 'center',
+  ActivityIndicatorStyle: {
+    position: 'absolute',
+    left:0,
+    right:0,
+    top:0,
+    bottom:0,
+    justifyContent: 'center',
+    alignItems:'center'
   }
 })
